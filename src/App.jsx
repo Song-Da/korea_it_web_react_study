41b9e95bea2ac34906c33316efce4642f43e2288
Song-Da <songowod@naver.com>
2025-08-06 22:24:56 +0900

    Init: first study

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import BasicJsx from "./components/BasicJsx/BasicJsx";
+import BasicProps from "./components/BasicProps/BasicProps";
+import BasicReact from "./components/BasicReact/BasicReact";
+
+function App() {
+  return (
+    <>
+      {/* <BasicReact /> */}
+      {/* <BasicJsx /> */}
+      <BasicProps />
+    </>
+  );
+}
+
+export default App;
